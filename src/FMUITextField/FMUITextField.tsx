1793ec106bdd228de6bebc98523b09e3db63cd28
LassazVegaz <lasindunuwanga@gmail.com>
2023-09-29 17:48:20 +0530

    first commit

---
+++ src/FMUITextField/FMUITextField.tsx
@@ -0,0 +1,34 @@
+import React, { useMemo } from "react";
+import TextField, { TextFieldProps } from "@mui/material/TextField";
+import { FormikProps } from "formik";
+
+export type FMUITextFieldProps<T> = TextFieldProps & {
+	form: FormikProps<T>;
+	name: string & keyof T;
+};
+
+const FMUITextField = <T,>(props: FMUITextFieldProps<T>) => {
+	const { name, form, ...rest } = props;
+
+	const error = useMemo(() => {
+		const hasError = form.touched[name] && Boolean(form.errors[name]);
+		return {
+			hasError,
+			message: hasError ? form.errors[name]!.toString() : null,
+		};
+	}, []);
+
+	return (
+		<TextField
+			name={name}
+			onChange={form.handleChange}
+			onBlur={form.handleBlur}
+			value={form.values[name]}
+			error={error.hasError}
+			helperText={error.message}
+			{...rest}
+		/>
+	);
+};
+
+export default FMUITextField;
