88c27ba5494ce8d9e25698bf2592b2c30ca7b670
LassazVegaz <lasindunuwanga@gmail.com>
2023-09-30 00:00:18 +0530

    idk 🙂

--- src/FMUITextField/FMUITextField.tsx
+++ src/FMUITextField/FMUITextField.tsx
@@ -1,14 +1,21 @@
 import { useEffect, useMemo } from "react";
-import TextField, {
-	type TextFieldProps,
-} from "@mui/material/TextField/TextField";
+import { TextField, type TextFieldProps } from "@mui/material";
 import { type FormikProps } from "formik";
 
+/**
+ * Props of `FMUITextField`
+ * @template T Type of formik form values
+ */
 export type FMUITextFieldProps<T> = TextFieldProps & {
+	/** Formik form */
 	form: FormikProps<T>;
+	/** Name of the field. This should be a key from form values */
 	name: string & keyof T;
 };
 
+/**
+ * MUI TextField with Formik integration. `name` and `form` are required.
+ */
 const FMUITextField = <T,>(props: FMUITextFieldProps<T>) => {
 	const { name, form, ...rest } = props;
 
