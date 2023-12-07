20ffb79399d8add3cedce3f6e380cc9d732e0889
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-07 20:41:29 +0530

    use client in TextField

--- src/FMUITextField/FMUITextField.tsx
+++ src/FMUITextField/FMUITextField.tsx
@@ -1,6 +1,7 @@
+"use client";
 import { useMemo } from "react";
 import TextField, {
-	type TextFieldProps,
+  type TextFieldProps,
 } from "@mui/material/TextField/TextField";
 import { type FormikProps } from "formik/dist/types";
 
@@ -9,10 +10,10 @@ import { type FormikProps } from "formik/dist/types";
  * @template T Type of formik form values
  */
 export type FMUITextFieldProps<T> = TextFieldProps & {
-	/** Formik form */
-	form: FormikProps<T>;
-	/** Name of the field. This should be a key from form values */
-	name: string & keyof T;
+  /** Formik form */
+  form: FormikProps<T>;
+  /** Name of the field. This should be a key from form values */
+  name: string & keyof T;
 };
 
 /**
@@ -20,27 +21,27 @@ export type FMUITextFieldProps<T> = TextFieldProps & {
  * @template T Type of formik form values
  */
 const FMUITextField = <T,>(props: FMUITextFieldProps<T>) => {
-	const { name, form, ...rest } = props;
+  const { name, form, ...rest } = props;
 
-	const error = useMemo(() => {
-		const hasError = form.touched[name] && Boolean(form.errors[name]);
-		return {
-			hasError,
-			message: hasError ? form.errors[name]!.toString() : null,
-		};
-	}, [form.errors, form.touched, name]);
+  const error = useMemo(() => {
+    const hasError = form.touched[name] && Boolean(form.errors[name]);
+    return {
+      hasError,
+      message: hasError ? form.errors[name]!.toString() : null,
+    };
+  }, [form.errors, form.touched, name]);
 
-	return (
-		<TextField
-			name={name}
-			onChange={form.handleChange}
-			onBlur={form.handleBlur}
-			value={form.values[name]}
-			error={error.hasError}
-			helperText={error.message}
-			{...rest}
-		/>
-	);
+  return (
+    <TextField
+      name={name}
+      onChange={form.handleChange}
+      onBlur={form.handleBlur}
+      value={form.values[name]}
+      error={error.hasError}
+      helperText={error.message}
+      {...rest}
+    />
+  );
 };
 
 export default FMUITextField;
